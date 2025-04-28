63891a1bbc5087df38ee509a27842ca6cb8e3a4a
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-04-28 12:34:22 +0400

    fix: increase body-max-line-length limit to 2000 characters in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 200] // устанавливаем новый лимит 200 символов
+    'body-max-line-length': [2, 'always', 2000] // устанавливаем новый лимит 200 символов
   }
 };
