c9ba7f7e4dc75d0e66886c60d933a3d095a946ff
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-05-11 13:57:05 +0400

    refactor: remove unused admin components and related files

--- src/components/admin/users/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ShowGuesser } from 'react-admin';
-import { UserGroupIcon } from '@heroicons/react/20/solid';
-import UserList from './UserList';
-
-export default {
-  list: UserList,
-  show: ShowGuesser,
-  icon: () => <UserGroupIcon className="w-5 h-5" />,
-};
